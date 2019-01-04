f886bc36c160c5fa3f3fe480b4f380e46520b651
Angel Kurten <angelkurten@hotmail.com>
2019-01-04 18:01:19 -0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    broadcast(new \App\Events\WebsocketDemoEvent('some data'));
+    return view('welcome');
+});
+
+Route::get('/chats', 'ChatsController@index')->name('chat');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/messages', 'ChatsController@fetchMessages')->name('messages');
+Route::post('/messages', 'ChatsController@sendMessage')->name('messages-post');
